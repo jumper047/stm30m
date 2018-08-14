f4364ad052a2c757be10d7417ad3c595f2ff3404
jumper047 <jumper047@gmail.com>
2018-08-14 07:40:10 +0000

    add signals and functions

--- app/mainwindow.py
+++ app/mainwindow.py
@@ -8,6 +8,12 @@ Ui_MainWindow_main, QMainWindow_main = uic.loadUiType('app/mainwindow.ui')
 
 class Main(QMainWindow_main, Ui_MainWindow_main):
 
+    setThreshold1 = pyqtSignal(float, int)
+    setThreshold2 = pyqtSignal(float, int)
+    calibrateZero = pyqtSignal(float)
+    calibrateEnd = pyqtSignal(float)
+    setAddress = pyqtSignal(int)
+    
     def __init__(self):
         super(Main, self).__init__()
         self.setupUi(self)
@@ -32,4 +38,22 @@ class Main(QMainWindow_main, Ui_MainWindow_main):
         self.labelThreshold2.setText(u"Порог 2: " + str(data[2]) + u"% НКПР")
 
     def addModbusEvent(self, event):
-        self.textEditEventLog.insertPlainText(event + "\n")
+        self.textEditEventLog.insertPlainText(event + "\n")    
+         
+    def _setThreshold(self):
+        block_flag = 1 if self.checkBox.isChecked() else 0
+        threshold = float(self.lineEdit_2.text())
+        if self.comboBox_2.currentIndex() == 0:
+            self.setThreshold1.emit(threshold, block_flag)
+        elif self.comboBox_2.currentIndex() == 1:
+            self.setThreshold2.emit(threshold, block_flag)
+    
+    def _calibrate(self):
+        pgs_concentrations = float(self.lineEdit_3.text())
+        if self.comboBox_3.currentIndex() == 0:
+            self.calibrateZero.emit(pgs_concentration)
+        elif self.combobox_3.currentIndex() == 1:
+            self.calibrateEnd.emit(pgs_concentration)
+    
+    def _setAddress(self):
+        self.setAddress.emit(int(self.lineEdit_1.text()))
